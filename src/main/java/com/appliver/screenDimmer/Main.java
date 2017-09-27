840a13b53739891b5c350c1e7f345aa73b2e79ad
Gaurav Maheshwari <gauravmaheshwari57@gmail.com>
2017-09-27 00:38:01 +0000

    new changes

--- src/main/java/com/appliver/screenDimmer/Main.java
+++ src/main/java/com/appliver/screenDimmer/Main.java
@@ -3,12 +3,16 @@ package com.appliver.screenDimmer;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import com.splunk.mint.Mint;
 
 public class Main  extends Activity {
     
      @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        Mint.initAndStartSession(this.getApplication(), "2866a5ad");
+        
         Intent svc = new Intent(this, OverlayShowingService.class);
         startService(svc);
         finish();
